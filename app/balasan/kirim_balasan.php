a8e7d9c60ff4aa17f8b0fa3b13a2b28d490c67a9
Raguna9 <Dickyuzumaki73@gmail.com>
2023-03-27 09:03:16 +0800

    fix bug kirim balasan

--- app/balasan/kirim_balasan.php
+++ app/balasan/kirim_balasan.php
@@ -9,41 +9,41 @@ if (isset($_POST["submit"])) {
     if (tambahBalasan($_POST) > 0) {
         echo "
         <script>
-            alert('pesan terkirim');
-            document.location.reload();
+            alert('Balasan terkirim');
+            window.location.href = 'full_artikel.php?id=$id_artikel';
         </script>
         ";
     } else {
         echo "
         <script>
-            alert('pesan tidak terkirim');
-            document.location.reload();
+            alert('Balasan tidak terkirim');
+            window.location.href = 'full_artikel.php?id=$id_artikel';
         </script>
         ";
     }
 }
 
 ?>
-    <div class="card mt-5">
-        <div class="card-header bg-success text-white">
-            <strong>Kirimkan balasan mengenai artikel ini</strong>
-        </div>
-        <div class="card-body">
-            <form method="POST" action="" enctype="multipart/form-data">
-                <input type="hidden" name="id_artikel" value="<?php echo $_GET['id']; ?>">
-                <div class="mb-3">
-                    <label for="nama_balasan" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="nama_balasan" name="nama_balasan" required>
-                </div>
-                <div class="mb-3">
-                    <label for="email_balasan" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email_balasan" name="email_balasan" required>
-                </div>
-                <div class="mb-3">
-                    <label for="isi_balasan" class="form-label">Balasan</label>
-                    <textarea class="form-control" id="isi_balasan" name="isi_balasan" rows="3" required></textarea>
-                </div>
-                <button class="btn btn-success" type="submit" name="submit">Kirim</button>
-            </form>
-        </div>
+<div class="card mt-5">
+    <div class="card-header bg-success text-white">
+        <strong>Kirimkan balasan mengenai artikel ini</strong>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="" enctype="multipart/form-data">
+            <input type="hidden" name="id_artikel" value="<?php echo $_GET['id']; ?>">
+            <div class="mb-3">
+                <label for="nama_balasan" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="nama_balasan" name="nama_balasan" required>
+            </div>
+            <div class="mb-3">
+                <label for="email_balasan" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email_balasan" name="email_balasan" required>
+            </div>
+            <div class="mb-3">
+                <label for="isi_balasan" class="form-label">Balasan</label>
+                <textarea class="form-control" id="isi_balasan" name="isi_balasan" rows="3" required></textarea>
+            </div>
+            <button class="btn btn-success" type="submit" name="submit">Kirim</button>
+        </form>
+    </div>
 </div>
